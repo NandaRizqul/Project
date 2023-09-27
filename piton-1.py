84ac569aa68a3f0fbd18e2d6f36d96295e2ed9fc
Nanda Rizqul <decade262@gmail.com>
2023-09-27 12:55:44 +0700

    ini program versi 1

---
+++ piton-1.py
@@ -0,0 +1,14 @@
+
+Largest_number = -999999999
+
+number = int(input("Enter a number or type -1 to stop: "))
+
+while number !=-1:
+
+    If number > largest_number:
+       
+       largest_number = number
+ 
+    number = int(input("Enter a number or type -1 to stop: "))
+
+print("The largest number is:", largest_number)
